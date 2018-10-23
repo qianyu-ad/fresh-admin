bf16d0e33de26b07430d5fe2a50fa069d4716ede
cangmean <cangmean@outlook.com>
2018-10-23 20:45:19 +0800

    添加antd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App)
-}).$mount('#app')
+})
